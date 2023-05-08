9ba9d037db09d96649e6d3f6ccd824156214a4c8
ramses2099 <ramses2099@gmail.com>
2023-05-08 05:54:21 -0400

    commit today

--- rearrange_test.py
+++ rearrange_test.py
@@ -6,6 +6,9 @@ class TestRearrange(unittest.TestCase):
     def test_basic(self):
         testcase = "Lovelace, Ada"
         expected = "Ad Lovelace"
+        
+        
+        
 
 
 # Convertí a la villana en mi mascota 3
